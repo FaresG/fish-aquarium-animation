1b143f42d4e77de7e7f2aafe8e1a2340a477b1b5
Fares Gdoura <gdourafares@gmail.com>
2024-07-21 17:59:15 +0100

    build fish component

--- src/Fish.vue
+++ src/Fish.vue
@@ -1,17 +1,47 @@
 <script setup>
-import {computed, ref, watch} from "vue";
+import { computed, ref } from "vue";
 
 const fishWidth = 120
+// Assume that it will never start with 0, 0 coords
 const xPosition = ref(0)
-const yPosition = ref(0)
-const position = computed(() => {
+const yPosition = ref(20)
+const positionStyle = computed(() => {
   return `top: ${yPosition.value}%; left: ${xPosition.value}%;`
 })
 
-watch(xPosition, (newValue) => {
-  // deduct fish width
-  newValue -= fishWidth
-})
+const forwardX = ref(true)
+const forwardY = ref(true)
+const updateX = () => {
+  switch (xPosition.value) {
+    case 0:
+      forwardX.value = true
+      break
+    case 100:
+      forwardX.value = false
+      break
+  }
+
+  forwardX.value ? xPosition.value++ : xPosition.value--
+}
+const updateY = () => {
+  switch (yPosition.value) {
+    case 0:
+      forwardY.value = true
+      break
+    case 100:
+      forwardY.value = false
+      break
+  }
+  forwardY.value ? yPosition.value++ : yPosition.value--
+}
+const updateCoords = () => {
+  updateX()
+  updateY()
+}
+
+setInterval(() => {
+  updateCoords()
+}, 50)
 </script>
 
 <template>
@@ -20,7 +50,7 @@ watch(xPosition, (newValue) => {
         src="../public/golden-purple-fish.png"
         alt="goldfish"
         :class="`w-[${fishWidth}px] absolute`"
-        :style="position"
+        :style="positionStyle"
     />
   </Transition>
 </template>
